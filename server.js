0a3fca92cab40987b4191b8b906cb946bf844edf
hardcommitoneself <hardcommitoneself@gmail.com>
2022-12-12 01:33:20 -0800

    init commit & payment integration complete

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const stripe = require("stripe")("sk_test_51ME4ooG5GHjXDN1VXXRRK25pW2p0NKB7xl99Z33kif51cj0Mw3PuYNEKs4IpLhPYsSrXlyFiZFFlIAaxG6y2qTvE00WraE9M0t");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors({}));
+
+const calculateOrderAmount = items => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return items.reduce((a, c) => a + c.price * c.quantity, 0) * 100;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body;
+
+    if(!items.length) {
+      return res.status(400).send({
+        msg: 'no items'
+      });
+    }
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items),
+      currency: "usd"
+    });
+  
+    res.send({
+      clientSecret: paymentIntent.client_secret
+    });
+});
+
+app.listen(4242, () => console.log('Node server listening on port 4242!'));
